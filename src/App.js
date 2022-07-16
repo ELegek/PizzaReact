91f9be1a5995e7332dbf6fb9223248c4bbace871
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2022-07-16 22:59:57 +0300

    creact component categoris

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import Header from './Header.jsx';
+import { Header, Categories } from './components';
 
 function App() {
 	return (
 		<div className='wrapper'>
 			<Header />
+
 			<div className='content'>
 				<div className='container'>
 					<div className='content__top'>
-						<div className='categories'>
-							<ul>
-								<li className='active'>Все</li>
-								<li>Мясные</li>
-								<li>Вегетарианская</li>
-								<li>Гриль</li>
-								<li>Острые</li>
-								<li>Закрытые</li>
-							</ul>
-						</div>
+						<Categories items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']} />
 						<div className='sort'>
 							<div className='sort__label'>
 								<svg width='10' height='6' viewBox='0 0 10 6' fill='none' xmlns='http://www.w3.org/2000/svg'>
