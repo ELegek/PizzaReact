95ff5cf20305acdf6584442dbc13dc2534a39e4b
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2022-07-17 11:41:26 +0300

    add useState component Categoris

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import classNames from 'classnames';
 
-function Button({ onClick, className, outline, children }) {
+function Button({ className, outline, children }) {
 	return (
 		<button
-			onclick={onClick}
 			className={classNames('button', className, {
 				'button--outline': outline,
 			})}>
