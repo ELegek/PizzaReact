1a82a8665e9ad934810bd03ab566c00002de75ac
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2022-10-27 19:16:02 +0300

    Получаем данные с mockAPI

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ import Sort from './components/Sort';
 import Categories from './components/Categories';
 import PizzaBlock from './components/PizzaBlock';
 
-// Json пицц
-import pizzas from './assets/pizzas.json';
-
 function App() {
+	const [item, setItem] = React.useState([]);
+
+	React.useEffect(() => {
+		// Получаем пиццы с сервера
+		fetch('https://635a8f9a38725a1746ca0088.mockapi.io/items')
+			.then((res) => {
+				return res.json();
+			})
+			.then((json) => {
+				// Сохраняем массив пицц в стейт
+				setItem(json);
+			});
+	}, []);
+
 	return (
 		<div className='wrapper'>
 			<Header />
@@ -21,7 +32,7 @@ function App() {
 					</div>
 					<h2 className='content__title'>Все пиццы</h2>
 					<div className='content__items'>
-						{pizzas.map((obj) => (
+						{item.map((obj) => (
 							<PizzaBlock key={obj.id} {...obj} />
 						))}
 					</div>
